553ac51a0e459ffe22947bd8e6ae966c358f5fd7
Cringe Dealer <a.alahov@ya.ru>
2021-10-20 00:00:25 +0300

    Time compare

--- 04/mainwindow.cpp
+++ 04/mainwindow.cpp
@@ -4,6 +4,7 @@
 #include <QColor>
 #include <QColorDialog>
 #include <vector>
+#include <fstream>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -78,24 +79,35 @@ void MainWindow::on_clear_clicked()
 void MainWindow::on_time_button_clicked()
 {
     ui->time->clear();
-
-    polygon *pol = new polygon(ui->fg_view->get_color(), ui->paint_view->get_color());
-    for (int i = 0; i < 250; i++) {
-        point *p = new point(rand() % ui->field->width(), rand() % ui->field->height());
-        pol->add_point(p, false, false);
+    std::ofstream out;
+    out.open("../table.csv");
+    if (!out.is_open()) {
+        return;
     }
-    pol->close();
+    out << "Len,th0,th1,th2,th4,th8,th16" << std::endl;
+    for (int n = 20; n < 30; n += 250) {
+        out << n;
 
+        polygon *pol = new polygon(ui->fg_view->get_color(), ui->paint_view->get_color());
+        for (int i = 0; i < n; i++) {
+            point *p = new point(rand() % ui->field->width(), rand() % ui->field->height());
+            pol->add_point(p, false, false);
+        }
+        pol->close();
 
-
-    double time;
-    ui->field->get_time(time, 0, pol);
-    ui->time->append(QString("Потоков: 0; Время: ") + QString::number(time) + QString(" сек"));
-    for (size_t th_count = 1; th_count < 32; th_count *= 2) {
-        ui->field->get_time(time, th_count, pol);
-        ui->time->append(QString("Потоков: ") + QString::number(th_count) + QString("; Время: ") + QString::number(time) + QString(" сек"));
+        double time;
+        ui->field->get_time(time, 0, pol);
+        ui->time->append(QString("Потоков: 0; Время: ") + QString::number(time) + QString(" сек"));
+        out << ',' << time;
+        for (size_t th_count = 1; th_count < 32; th_count *= 2) {
+            ui->field->get_time(time, th_count, pol);
+            ui->time->append(QString("Потоков: ") + QString::number(th_count) + QString("; Время: ") + QString::number(time) + QString(" сек"));
+            out << ',' << time;
+        }
+        out << std::endl;
+        delete pol;
     }
-    delete pol;
+    out.close();
 }
 
 void MainWindow::on_vertical_clicked()
